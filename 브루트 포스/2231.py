89bd4d4cf517e2bbdaf374b4123e5f44fa94ebea
Dodamking <power6120@naver.com>
2021-08-10 15:53:03 +0900

    정답

--- 브루트 포스/2231.py
+++ 브루트 포스/2231.py
@@ -2,6 +2,8 @@
 # 모든 경우를 시도하여 N의 생성자를 구하는 문제
 
 N = int(input())
+
+## 처음코드
 con = 1
 con_list = []
 
@@ -19,11 +21,12 @@ else:
     print(min(con_list))
 
 
+## 간결한 코드
 sangsungja = 0
 for i in range(1, N + 1):
-    digit_list = list(map(int, str(i)))
-print(sum(digit_list))
-#     hap = sum(digit_list)
-#     if N == i + hap:
-#         sangsungja = i
-# print(sangsungja)
+    digit_list = [int(j) for j in str(i)]
+    hap = sum(digit_list)
+    if N == i + hap:
+        sangsungja = i
+        break
+print(sangsungja)
