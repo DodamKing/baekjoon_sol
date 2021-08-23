8deb4e11ebf5b83279d56451d4ec148644d0af17
Dodamking <power6120@naver.com>
2021-08-23 23:02:44 +0900

    factorial

---
+++ 알고리즘기초1_2/1676.py
@@ -0,0 +1,21 @@
+# 팩토리얼 0의 개수 실버 4
+
+n = int(input())
+f = 1
+factorial = []
+count = 0
+for i in range(1, n+1):
+    f = f * i
+# print(f)
+for j in str(f):
+    factorial.append(j)
+# print(factorial)
+while True:
+    if factorial[-1] == '0':
+        factorial.pop(-1)
+        count += 1
+        print(factorial)
+    else:
+        break
+# print(factorial)
+print(count)
